d3ae8312b2fece093dde4a5a89601224eae55330
Hugo Romat <romathugo@microsoft.com>
2022-09-12 17:43:56 +0200

    x

--- sketch_sep10a/sketch_sep10a.ino
+++ sketch_sep10a/sketch_sep10a.ino
@@ -28,13 +28,7 @@ void setup()
   Serial.println(WiFi.softAPIP());
 
   server.on("/", handle_OnConnect);
-  
-  server.on("/cocktail", HTTP_GET, [](AsyncWebServerRequest *request){
-      if (request->args() == 0) // no arguments attached -> STATUS_BAD_REQUEST
-          return request->send(400, "text/plain", F("ERROR: Bad or no arguments"));
-     uint8_t dhtSensor = request->arg("cocktailName").toInt(); 
-     request->send(200, "text/plain", readDSTemperature(dhtSensor).c_str());
- });
+  server.on("/cocktail", handleSpecificArg);
 
   server.begin();
 }
@@ -46,17 +40,17 @@ void loop() {
   delay(500);
 }
 
-void readDSTemperature(uint8_t sensorNumber){
-
-  Serial.println(sensorNumber);
-//  switch(sensorNumber){
-//      case 1:
-//          Serial.println("Someonte has connected");
-//          break;
-//      ....        
-//      }
-   }
-
+void handleSpecificArg() { 
+  String message = "";
+  if (server.arg("cocktailName")== ""){     //Parameter not found
+    message = "Temperature Argument not found";
+  } else {     //Parameter found
+    message = "Temperature Argument = ";
+    message += server.arg("cocktailName");     //Gets the value of the query parameter 
+  }
+  Serial.println(message);
+  server.send(200, "text/plain", message);          //Returns the HTTP response
+}
 void handle_OnConnect() {
   Serial.println("Someonte has connected");
   server.send(200, "text/plain", "OK"); 

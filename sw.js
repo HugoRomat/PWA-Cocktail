71583b3df79e8801329fcc0850c9682ad41210d5
Hugo Romat <romathugo@microsoft.com>
2022-09-12 15:23:22 +0200

    x

---
+++ sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  "/images/mojito.jpg",
+  "/images/ricard.jpg",
+  "/images/whickycoca.jpg"
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
